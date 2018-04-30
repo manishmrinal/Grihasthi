c6e5bd596532631d43142e853229249308dfb4c9
Manish Mrinal <manishmrinalface@gmail.com>
2018-05-01 03:43:14 +0530

    Initial commit

---
+++ app/src/main/java/com/manish/grihasthi/LandingPage.java
@@ -0,0 +1,48 @@
+package com.manish.grihasthi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class LandingPage extends AppCompatActivity {
+
+
+    private TextView tv;
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+        tv=(TextView)findViewById(R.id.landing_header);
+        iv=(ImageView)findViewById(R.id.logo_home);
+
+        Animation anim= (Animation) AnimationUtils.loadAnimation(this,R.anim.landing_page_animation);
+        tv.startAnimation(anim);
+        iv.startAnimation(anim);
+        final Intent i=new Intent(this,MainLoginPage.class);
+        Thread timer =new Thread(){
+
+             public void run() {
+
+                 try {
+                    sleep(5000);
+                 } catch (InterruptedException e1) {
+                     e1.printStackTrace();
+                 } finally {
+
+                     startActivity(i);
+                     finish();
+                 }
+             }
+            };
+
+        timer.start();
+
+
+    }
+}
---
+++ app/src/main/java/com/manish/grihasthi/UpdateProduct.java
@@ -0,0 +1,13 @@
+package com.manish.grihasthi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdateProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_product);
+    }
+}
